3c88c924aa2fb2440802b678a53aad2cbe862ac7
Aditya Verma <122523624+AadiVerma@users.noreply.github.com>
2024-08-09 16:54:13 +0530

    Update Avatar.tsx

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,5 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import Image from "/image.jpg"
+import Image from "/image.png"
 export default function AvatarNav(){
    return (
     <Avatar>
